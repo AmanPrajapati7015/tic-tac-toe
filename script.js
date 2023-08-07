e9955af64a036f077b08ec2dadbe8288b32b429c
Aman <aman.prajapati7022@gmail.com>
2023-08-07 19:08:59 +0530

    click and display player-wise

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
+let Player = function(name, marker){
+    return {name, marker}
+}
+
+let Player1 = Player("Aman", "x");
+let Player2 = Player("Shiwani", "o");
+
+
+
+
 let GameBoard = (function (){
-    let gameBoard = [ "x", "o", "o","o",null , "x",null, "o", "x"]
+    let gameBoard = [ null, null, null, null, null, null, null, null, null ]
+
+    let currentPlayer = Player1;
+    function switchPlayer(){
+        currentPlayer = (currentPlayer==Player1) ? Player2 : Player1
+    }
+
+    function play(e){
+        let cellDiv =  e.target
+        dataIndex = +cellDiv.getAttribute("data-index")
+        if (!gameBoard[dataIndex]){
+            gameBoard[dataIndex] = currentPlayer.marker;
+            cellDiv.textContent = currentPlayer.marker
+            switchPlayer()
+        }
+        else{
+            alert("you can't change a filled cell")
+        }
+    }
+
 
     let displayBoard = function(){
         for (let i = 0; i<gameBoard.length; i++){
-            let selector = `.grid-${i+1}`
-            let cellDiv = document.querySelector(selector);
+            let cellDiv = document.querySelector(`.grid-${i+1}`);
+            cellDiv.addEventListener("click", play)
             cellDiv.textContent = gameBoard[i];
         }
     }
+
+
+
     return {displayBoard}
 })();
 
 GameBoard.displayBoard()
+
+
+
+
+
+
+
+
+
+
+
+
+
